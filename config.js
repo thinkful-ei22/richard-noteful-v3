8f94b697b51b593044a9c4f18d41935e5b11c54d
Richard Tom <tomrichard14@gmail.com>
2018-07-15 18:16:11 -0700

    updated configs

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful-test',
-  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user1:ASDF1234@ds151180.mlab.com:51180/noteful-v3',
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful-test',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user1:ASDF1234@ds151180.mlab.com:51180/noteful-v3',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
 };
